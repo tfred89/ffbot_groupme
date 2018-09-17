202dd92233ef51b2797b2464aead552cf2c31aa7
Trent Frederick <trentfrederick@gmail.com>
2018-09-17 13:57:01 -0500

    deploy changes

--- ff_bot/ff_bot.py
+++ ff_bot/ff_bot.py
@@ -363,7 +363,7 @@ if __name__ == '__main__':
         day_of_week='tue', hour=7, minute=30, start_date=ff_start_date, end_date=ff_end_date,
         timezone=myTimezone, replace_existing=True)
     sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], id='scoreboard1',
-        day_of_week='fri,mon', hour=11, minute=55, start_date=ff_start_date, end_date=ff_end_date,
+        day_of_week='fri,mon', hour=15, minute=01, start_date=ff_start_date, end_date=ff_end_date,
         timezone=myTimezone, replace_existing=True)
     sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], id='scoreboard2',
         day_of_week='sun', hour='16,20', start_date=ff_start_date, end_date=ff_end_date,
